7855139d55bc786a3c55c39e8fa75e9a4014c852
CodeWithSomesh <codewithsomesh@gmail.com>
2024-07-31 03:15:07 +0800

    Done with Banner and Navbar

---
+++ src/components/Banner.tsx
@@ -0,0 +1,20 @@
+
+const Banner = () => {
+  return (
+    <div className="py-3 text-center  bg-[linear-gradient(to_right,#fcd6ff,#29d8ff,#fffd80,#f89abf,#fcd6ff)]">
+      <div className="container">
+        <p className="font-semibold">
+          <span className="hidden sm:inline">
+            Introducing a completely redesigned interface -
+          </span>
+          <a href="#" className="underline underline-offset-4 px-2 font-bold italic">
+            Explore The Demo
+          </a>
+        </p>
+      </div>
+    </div>
+
+  )
+}
+
+export default Banner
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import logoImage from '../assets/images/logosaas.png';
+import MenuIcon from '../assets/icons/menu.svg';
+
+
+const Navbar = () => {
+  return (
+    <div className='bg-black'>
+      <div className="px-6">
+        <div className="py-4 flex items-center justify-between">
+          <div className='relative'>
+            <div className='absolute w-full top-2 bottom-0 bg-[linear-gradient(to_right,#f87bff,#fb92cf,#ffdd9b,#c2f0b1,#2fd8fe)] blur-md'></div>
+            <Image src={logoImage} alt="Saas logo" className="h-12 w-12 relative" />
+          </div>
+
+          <div className="border border-white border-opacity-30 h-10 w-10 inline-flex items-center justify-center rounded-lg sm:hidden">
+            <MenuIcon className="text-white" />
+          </div>
+
+          <nav className="hidden sm:flex gap-6 items-center">
+            <a href="#" className='text-opacity-60 hover:text-opacity-100 text-white'>About</a>
+            <a href="#" className='text-opacity-60 hover:text-opacity-100 text-white'>Features</a>
+            <a href="#" className='text-opacity-60 hover:text-opacity-100 text-white'>Updates</a>
+            <a href="#" className='text-opacity-60 hover:text-opacity-100 text-white'>Help</a>
+            <a href="#" className='text-opacity-60 hover:text-opacity-100 text-white'>Customers</a>
+            <button className='bg-white py-2 px-4 rounded-lg'>Get for free</button>
+          </nav>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
