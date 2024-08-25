d8bedd372918ae4363284e7b6101c263ee1de7ab
CodeWithSomesh <codewithsomesh@gmail.com>
2024-08-25 20:39:04 +0800

    Done with the entire site structure

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,3 +1,31 @@
+import helixImage from '../assets/images/helix2.png'
+import emojiStarImage from '../assets/images/emojistar.png'
+import Image from 'next/image';
+
 export const CallToAction = () => {
-  return null;
+  return (
+    <div className="bg-black text-white py-[72px] sm:py-24 text-center">
+      <div className="container max-w-xl relative">
+        <Image src={helixImage} alt='' 
+        className="absolute top-6 left-[calc(100%+36px)]" />
+
+        <Image src={emojiStarImage} alt='' 
+        className="absolute -top-[120px] right-[calc(100%+24px)]" />
+
+        <h2 className="font-bold text-5xl sm:text-6xl tracking-tighter">Get Instant Access</h2>
+
+        <p className="text-xl text-white/70 mt-5">
+          Celebrate the joy of accomplishment with an app designed 
+          to track your progress and motivate your efforts.
+        </p>
+
+        <form action="" className="mt-10 flex flex-col gap-2.5 max-w-sm mx-auto sm:flex-row">
+          <input type="email" placeholder="your@email.com" 
+          className="h-12 bg-white/20 rounded-lg px-5 font-medium placeholder:text-[#9ca3af] sm:flex-1" />
+          
+          <button className="bg-white text-black h-12 rounded-lg font-semibold px-5">Get Access</button>
+        </form>
+      </div>
+    </div>
+  );
 };
