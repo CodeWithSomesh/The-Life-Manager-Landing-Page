c604c412148b2f54da727e53c6ab9b3202ea1ce5
CodeWithSomesh <codewithsomesh@gmail.com>
2024-08-26 06:08:22 +0800

    Done with Logo Ticker Animation

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import acmeLogo from "../assets/images/acme.png";
 import quantumLogo from "../assets/images/quantum.png";
 import echoLogo from "../assets/images/echo.png";
@@ -5,6 +7,8 @@ import celestialLogo from "../assets/images/celestial.png";
 import pulseLogo from "../assets/images/pulse.png";
 import apexLogo from "../assets/images/apex.png";
 import Image from "next/image";
+import { motion } from 'framer-motion'
+
 
 const images = [
   { src: acmeLogo, alt: "Acme Logo" },
@@ -24,12 +28,19 @@ export const LogoTicker = () => {
           Trusted by the world&apos;s most innovative teams
         </h2>
 
-        <div className="overflow-hidden mt-9 before:content-[''] after:content-[''] 
+        <div className="flex overflow-hidden mt-9 before:content-[''] before:z-10 after:content-[''] 
         before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5
         relative before:left-0 after:right-0 before:top-0 after:top-0
         before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))]
         after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]" >
-          <div className="flex gap-16">
+          <motion.div className="flex gap-16 flex-none pr-16"
+          initial={{translateX: 0}}
+          animate={{translateX: "-50%"}}
+          transition={{
+            duration: 10,
+            ease: "linear",
+            repeat: Infinity,
+          }}>
             {images.map(({ src, alt }) => (
               <Image
                 key={alt}
@@ -38,7 +49,16 @@ export const LogoTicker = () => {
                 className="flex-none h-8 w-auto"
               />
             ))}
-          </div>
+
+            {images.map(({ src, alt }) => (
+              <Image
+                key={alt}
+                src={src}
+                alt={alt}
+                className="flex-none h-8 w-auto"
+              />
+            ))}
+          </motion.div>
         </div>
       </div>
     </div>
