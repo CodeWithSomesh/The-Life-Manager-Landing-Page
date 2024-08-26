0c730366eeb866501daa44c1edca228feac991a9
CodeWithSomesh <codewithsomesh@gmail.com>
2024-08-26 13:31:54 +0800

    Done with Features and Prodcut Showcase Section

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -1,7 +1,27 @@
+"use client"
+
 import Image from "next/image";
 import appScreen from "../assets/images/app-screen.png";
+import {motion, useScroll, useTransform} from 'framer-motion'
+import { useEffect, useRef } from "react";
 
 export const ProductShowcase = () => {
+
+  const appImage = useRef<HTMLImageElement>(null)
+  const {scrollYProgress} = useScroll({
+    target: appImage,
+    offset: ['start end', 'end end'],
+
+  })
+
+  useEffect(() => { //Can see the value in Dev Tools
+    scrollYProgress.on('change', (latestValue) => 
+      console.log('latestValue', latestValue))
+  })
+
+  const rotateX = useTransform(scrollYProgress, [0,1], [15,0])
+  const opacity = useTransform(scrollYProgress, [0,1], [0.1, 1])
+
   return (
     <div className="bg-black text-white bg-gradient-to-b from-black to-[#5D2CAB] py-[72px] sm:py-24">
       <div className="container">
@@ -15,9 +35,16 @@ export const ProductShowcase = () => {
           </p>
         </div>
 
-        <Image src={appScreen} alt="The product screenshot"
-        className="mt-14 mx-auto"
-        />
+        <motion.div 
+          style={{
+            opacity: opacity,
+            rotateX: rotateX,
+            transformPerspective: "800px",
+        }}>
+          <Image src={appScreen} alt="The product screenshot"
+          className="mt-14 mx-auto" ref={appImage}
+          />
+        </motion.div>
       </div>
     </div>
   );
